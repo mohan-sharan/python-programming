3ac155613ca80be8f64e4ba5ecf8b781aa797852
Sharan Mohan <36114089+mohan-sharan@users.noreply.github.com>
2018-04-21 21:41:07 -0500

    Update classes_4.py

--- classes_4.py
+++ classes_4.py
@@ -15,3 +15,6 @@ class Bugatti(Material):
         " made of {} and has a {} cylinder engine!" .format(self.topSpeed, self.materialType, self.numberCylinders))
 
 object = Bugatti()
+
+#OUTPUT
+#The Bugatti Veyron has a top speed of 254 mph. It is made of Carbon fiber and has a 16 cylinder engine!
