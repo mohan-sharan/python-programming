a4c03d6e0837b8b09e231059d2fdc977434d7dee
Sharan Mohan <36114089+mohan-sharan@users.noreply.github.com>
2018-04-25 11:22:53 -0500

    Add files via upload

---
+++ loops_1.py
@@ -0,0 +1,35 @@
+for letters in "Dragon Ball Z":
+    print(letters)
+
+# Empty lines
+print("")
+print("")
+
+word = "Python Programming"
+
+for letters in word:
+    # Only print out the letter P
+    if letters == "P":
+        print(letters)
+
+'''
+OUTPUT:
+D
+r
+a
+g
+o
+n
+ 
+B
+a
+l
+l
+ 
+Z
+
+
+P
+P
+'''		
+		
