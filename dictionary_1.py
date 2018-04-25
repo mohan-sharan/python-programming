9887dbe61c6f5b833d19e3b1dce70953fa968b5b
Sharan Mohan <36114089+mohan-sharan@users.noreply.github.com>
2018-04-24 20:38:17 -0500

    Add files via upload

---
+++ dictionary_1.py
@@ -0,0 +1,11 @@
+menu = {} # Empty dictionary
+menu = {"Alfredo Pasta": 14.50, "Taco": 1.8, "Burrito": 6.5, "Fried Rice": 6.70}
+
+print ("There are " + str(len(menu)) + " items on the menu.")
+print (menu)
+
+'''
+OUTPUT:
+There are 4 items on the menu.
+{'Fried Rice': 6.7, 'Taco': 1.8, 'Burrito': 6.5, 'Alfredo Pasta': 14.5}
+'''
