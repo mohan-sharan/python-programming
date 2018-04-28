dbfc15d8345c3824ebf013128ec919ce50c9c3c7
Sharan Mohan <36114089+mohan-sharan@users.noreply.github.com>
2018-04-27 23:41:39 -0500

    Update fibonacci_1.py

--- fibonacci_1.py
+++ fibonacci_1.py
@@ -19,7 +19,7 @@ The Fibonacci Sequence:
 0 1 1 2 3 5 8 	
 
 OUTPUT 2:
-Enter the number of terms you want in the sequence:17
+Enter the number of terms you want in the sequence: 17
 The Fibonacci Sequence:
 0 1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987 
 '''
