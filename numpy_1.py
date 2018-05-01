31c5268ce7899824fdaecacc31b8d59c2862f083
Sharan Mohan <36114089+mohan-sharan@users.noreply.github.com>
2018-05-01 17:59:39 -0500

    Add files via upload

---
+++ numpy_1.py
@@ -0,0 +1,44 @@
+#Solving a Linear System
+#A.x = b
+#x = A^(-1).b
+
+
+import numpy as np
+
+A = np.array([[4,5],[6,7]])
+
+print(A)
+
+'''
+OUTPUT:
+[[4 5]
+ [6 7]]
+'''
+
+b = np.array([1,2])
+
+print(b)
+
+'''
+OUTPUT:
+[1, 2]
+'''
+
+x = np.linalg.inv(A).dot(b)
+
+print(x)
+
+'''
+OUTPUT:
+[ 1.5, -1. ]
+'''
+
+x = np.linalg.solve(A, b)
+
+print(x)
+
+'''
+OUTPUT:
+[ 1.5, -1. ]
+''' 
+
