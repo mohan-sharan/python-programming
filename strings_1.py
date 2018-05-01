1d4f9e0a229215f53f50497675a8d8831b2b9b1b
Sharan Mohan <36114089+mohan-sharan@users.noreply.github.com>
2018-04-30 23:25:34 -0500

    Add files via upload

---
+++ strings_1.py
@@ -0,0 +1,28 @@
+myString = "Anyone"
+print(myString[0])
+print(myString[1])
+print(myString[2])
+print(myString[3])
+print(myString[4])
+print(myString[5])
+print("The length of the string is:", len(myString))
+
+
+print(myString + " can learn" + " to code!")
+
+pi = 3.14
+myText = "The value of pi is " + str(pi)
+print(myText)
+
+'''
+OUTPUT:
+A
+n
+y
+o
+n
+e
+The length of the string is: 6
+Anyone can learn to code!
+The value of pi is 3.14
+'''
