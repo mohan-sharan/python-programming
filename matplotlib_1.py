e65ad546250cba980ead12ccd6994e56fcc6d4ff
Sharan Mohan <36114089+mohan-sharan@users.noreply.github.com>
2018-05-01 19:23:45 -0500

    Add files via upload

---
+++ matplotlib_1.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+a = np.linspace(0,10,100)
+
+b = np.cos(a)
+
+plt.plot(x,y)
+
+plt.xlabel("Time")
+plt.ylabel("A function of time")
+plt.title("Example Chart")
+
+plt.show()
+
