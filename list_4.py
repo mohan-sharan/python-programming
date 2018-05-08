5a1a3fc7bd65a2ec8b46f81f766260476096476f
Sharan Mohan <36114089+mohan-sharan@users.noreply.github.com>
2018-05-07 23:00:16 -0500

    Add files via upload

---
+++ list_4.py
@@ -0,0 +1,24 @@
+myList = ['T', 'O', 'P', '', 'G', 'E', 'A', 'R']
+i = 0
+while i < len(myList):
+    print(myList[i], end=" ")
+    i += 1
+
+#OUTPUT: T O P  G E A R 	
+	
+#append() method
+myList.append("!")
+print("\nUpdated List: ", myList)
+
+#OUTPUT: Updated List:  ['T', 'O', 'P', '', 'G', 'E', 'A', 'R', 'R']
+
+#pop() method
+print(myList.pop()) #removes the last element from the list
+
+#OUTPUT: R
+
+#remove() method
+myList.remove('T')
+print("Updated List: ", myList)
+
+#OUTPUT: Updated List:  ['O', 'P', '', 'G', 'E', 'A', 'R']
