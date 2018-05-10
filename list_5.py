04067d257557ea4aef28f1f8616dab4bb0febcb1
Sharan Mohan <36114089+mohan-sharan@users.noreply.github.com>
2018-05-09 19:55:00 -0500

    Add files via upload

---
+++ list_5.py
@@ -0,0 +1,12 @@
+#Binary Representation (1-10)
+
+myList1 = [0b0001, 0b0010, 0b0011, 0b0100, 0b0101, 0b0110, 0b0111, 0b1000, 0b1001, 0b1010]
+print("List 1:", myList1)
+
+#OUTPUT - List 1: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+#bin()
+myList2 = [bin(1), bin(2), bin(3), bin(4), bin(5), bin(6), bin(7), bin(8), bin(9), bin(10)]
+print("List 2:", myList2)
+
+#OUTPUT - List 2: ['0b1', '0b10', '0b11', '0b100', '0b101', '0b110', '0b111', '0b1000', '0b1001', '0b1010']
