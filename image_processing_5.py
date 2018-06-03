1f985908bcfee8285aa38799d5cb270391a24976
Sharan Mohan <36114089+mohan-sharan@users.noreply.github.com>
2018-06-02 19:02:46 -0500

    Add files via upload

---
+++ image_processing_5.py
@@ -0,0 +1,25 @@
+from PIL import Image
+
+myImage1 = Image.open("wing_zero.jpg")
+myImage1.show()
+print(myImage1.size)
+#OUTPUT: (1280, 720)
+
+myImage2 = Image.open("endless_waltz.jpg")
+print(myImage2.size)
+#OUTPUT: (1440, 1080)
+
+size = 200, 200
+myImage2.thumbnail(size)
+myImage2.show()
+print(myImage2.size)
+#OUTPUT: (200, 150)
+
+myImage_copy = myImage1.copy()
+
+image_position = (25, 25)
+
+myImage_copy.paste(myImage2, image_position)
+size_1 = 900, 900
+myImage_copy.thumbnail(size_1)
+myImage_copy.show()
