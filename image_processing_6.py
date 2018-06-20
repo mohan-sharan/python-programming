48a2f254b976090c871d73dbb0ae9defbdd2078b
Sharan Mohan <36114089+mohan-sharan@users.noreply.github.com>
2018-06-19 19:57:52 -0500

    Add files via upload

---
+++ image_processing_6.py
@@ -0,0 +1,24 @@
+from PIL import Image, ImageDraw, ImageFont
+
+myImage = Image.open("vegito.png").convert('RGBA')
+size = 800, 800
+myImage.thumbnail(size)
+myImage.show()
+
+#Create a transparent text color image for the text
+myText = Image.new('RGBA', myImage.size, (255,255,255,0))
+
+#Accessing font types
+fnt1 = ImageFont.truetype('Pillow/Tests/fonts/DejaVuSans.ttf', 35)
+fnt2 = ImageFont.truetype('Pillow/Tests/fonts/FreeMono.ttf', 30)
+
+d = ImageDraw.Draw(myText)
+
+#Draw the text
+d.text((665,400), "Vegito", font=fnt1, fill=(255,255,255))
+#White Color: (R,G,B) - (255,255,255)
+d.text((10,10), "End Of The World Blast", font=fnt2, fill=(0,0,0))
+#Black color: (R,G,B) - (0,0,0)
+
+outputImage = Image.alpha_composite(myImage, myText)
+outputImage.show()
